6eae9011bfce9352147ed80af7f0d7fb813449f9
Jonas Castro <jonascastro128@gmail.com>
2020-07-27 11:57:42 -0300

    update mail

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -29,11 +29,10 @@ class SendForgotPasswordService {
       throw new AppError('User does not exists.');
     }
 
-    const token = await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
     await this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido:${token}`,
+     to:{},
     );
   }
 }
