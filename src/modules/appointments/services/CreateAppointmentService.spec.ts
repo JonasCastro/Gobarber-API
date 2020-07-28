0e0a4449c36b165362d8fd11a87e5522cd1c59ac
Jonas Castro <instjns@outlook>
2020-07-28 17:00:08 -0300

    appointment creation

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -18,6 +18,7 @@ describe('CreateAppointment', () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '12121212',
+      user_id: '12121212',
     });
 
     expect(appointment).toHaveProperty('id');
@@ -30,12 +31,14 @@ describe('CreateAppointment', () => {
     await createAppointment.execute({
       date: appointmentDate,
       provider_id: '12121212',
+      user_id: '12121212',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '12121212',
+        user_id: '12121212',
       }),
     ).rejects.toBeInstanceOf(AppErros);
   });
