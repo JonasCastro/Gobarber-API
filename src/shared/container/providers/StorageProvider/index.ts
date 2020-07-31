24ed40029cc065579a002bdc9062b3d6c3a4f5e7
Jonas Castro <instjns@outlook>
2020-07-31 16:46:07 -0300

    Upload files to Amazon S3

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,14 +1,17 @@
 import { container } from 'tsyringe';
 
+import uploadConfig from '@config/upload';
 import IStorageProvider from './models/IStorageProvider';
 
 import DiskStorageProvider from './implementations/DiskStorageProvider';
+import S3SStorageProvider from './implementations/S3SStorageProvider';
 
-const provider = {
-  diskStorage: DiskStorageProvider,
+const providers = {
+  disk: DiskStorageProvider,
+  s3: S3SStorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  provider.diskStorage,
+  providers[uploadConfig.driver],
 );
